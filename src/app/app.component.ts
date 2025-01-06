cfd50ef36ae78aa9829fc0d223270f6c7d9c13c9
Diego de la Jara <d.delajaral@gmail.com>
2025-01-06 16:10:57 -0300

    feat: :sparkles: Google maps working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { GoogleMapsModule } from '@angular/google-maps';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [GoogleMapsModule, MatInputModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'real-time-map';
+  address: string = '';
+  center: google.maps.LatLngLiteral = { lat: -33.4489, lng: -70.6693 }; // Santiago, Chile
+  zoom: number = 12;
+  mapOptions: google.maps.MapOptions = {
+    mapTypeId: 'roadmap',
+    scrollwheel: true,
+    disableDoubleClickZoom: false,
+    maxZoom: 15,
+    minZoom: 8,
+  };
+  markers: google.maps.LatLngLiteral[] = [];
+
+  sendAddress() {
+    // Implement geocoding and socket.io logic here
+  }
 }
